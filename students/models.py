10af12153a30c5d380c5332c439ddf0a546b1c69
Barasa2023 <officialbarasa@gmail.com>
2025-07-25 11:20:39 +0300

    Feat: Create student list view

--- students/models.py
+++ students/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from academics.models import ClassRoom
 
 from sms.settings import AUTH_USER_MODEL
 
 class Student(models.Model):
     user = models.OneToOneField(AUTH_USER_MODEL, on_delete=models.CASCADE)
     adm_no = models.CharField(max_length=100)
-    grade = models.CharField(max_length=15)
+    grade = models.ForeignKey(ClassRoom, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}"
--- students/views.py
+++ students/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth import get_user_model
+from .models import Student
 
 # Create your views here.
+class StudentList(ListView):
+    '''List all students'''
+    model = Student
+    template_name = 'students/student_list.html'
+    context_object_name = 'students'
+
+    def get_queryset(self):
+        return Student.objects.select_related('user').all()
