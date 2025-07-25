dcc1a226ef2c95b1df8fd33b5bbb714d3cd76236
Barasa2023 <officialbarasa@gmail.com>
2025-07-25 11:05:45 +0300

    Register academic app in the admin

--- academics/admin.py
+++ academics/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ClassRoom, Subject
 
 # Register your models here.
+@admin.register(ClassRoom)
+class ClassRoomAdmin(admin.ModelAdmin):
+    list_display = ['name', 'teacher', 'capacity']
