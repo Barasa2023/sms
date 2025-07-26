b382b16337ad5dbfe32fd62c9a6b76c91e38e66e
Barasa2023 <officialbarasa@gmail.com>
2025-07-26 13:30:49 +0300

    update fee type field for student

--- students/models.py
+++ students/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from academics.models import ClassRoom
+from finance.models import FeeType
 
 #from sms.settings import USER_AUTH_MODEL
 
@@ -10,9 +11,10 @@ class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     adm_no = models.CharField(max_length=100)
     grade = models.ForeignKey(ClassRoom, on_delete=models.CASCADE)
+    fee_type = models.ManyToManyField(FeeType, blank=True)
     total_fees = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    
+
 
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}"
