543866ba1ab2ae8728b053558e3f3c67e90acda0
Barasa2023 <officialbarasa@gmail.com>
2025-07-26 11:22:16 +0300

    Feat: Create Fee forms

--- finance/models.py
+++ finance/models.py
@@ -6,10 +6,16 @@ class FeeType(models.Model):
     description = models.TextField(blank=True, null=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
 
+    def __str__(self):
+        return self.name
+
 class Fee(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     fee_type = models.ForeignKey(FeeType, on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.fee_type
 
 class Payment(models.Model):
     fee = models.ForeignKey(Fee, on_delete=models.CASCADE)
