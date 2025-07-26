6d562031d16f47042b7d5cffa294574e1b32796c
Barasa2023 <officialbarasa@gmail.com>
2025-07-26 12:57:55 +0300

    Feat: Add fee field to user

--- finance/forms.py
+++ finance/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.forms import ModelForm
 from finance.models import FeeType, Fee
-from students.models import Student, Payment
+from students.models import Student
+from finance.models import Payment
 
 class FeeTypeForm(ModelForm):
     class Meta:
@@ -12,7 +13,4 @@ class FeeTypeForm(ModelForm):
 class FeePaymentForm(ModelForm):
     class Meta:
         model = Payment
-        fields = ['student', 'fee', 'amount', 'payment_date', 'payment_method', 'receipt_number']
-        widgets = {
-            'payment_date': forms.DateInput(attrs={'type': 'date'}),
-        }
+        fields = ['student', 'fee', 'amount', 'payment_method', 'receipt_number']
--- students/models.py
+++ students/models.py
@@ -10,6 +10,9 @@ class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     adm_no = models.CharField(max_length=100)
     grade = models.ForeignKey(ClassRoom, on_delete=models.CASCADE)
+    total_fees = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    
 
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}"
