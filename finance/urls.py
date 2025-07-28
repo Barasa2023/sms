7f40da0291778e65d9d3ae7a33709a2340a18bb7
Barasa2023 <officialbarasa@gmail.com>
2025-07-28 15:56:24 +0300

    Feat: Create fee update buttons

--- finance/urls.py
+++ finance/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('add/', views.CreateFeeView.as_view(), name='add_fee'),
     path('payments/', views.FeePaymentListView.as_view(), name='fee_payments'),
     path('payments/add/', views.FeePaymentView.as_view(), name='add_payment'),
+    path('payments/<int:pk>/edit/', views.FeeUpdateView.as_view(), name='fee_edit'),
+    path('payments/<int:pk>/delete/', views.FeeDeleteView.as_view(), name='fee_delete'),
 ]
