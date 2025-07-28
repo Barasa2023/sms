6eaeaca92484ee4043e22dcd183dd260752ffe47
Barasa2023 <officialbarasa@gmail.com>
2025-07-28 11:40:38 +0300

    Fix: Refactor code to use template inheritance

--- students/urls.py
+++ students/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = 'students'
 
 urlpatterns = [
-    path('', views.StudentList.as_view(), name = 'student_list'),
-    path('add/', views.StudentCreate.as_view(), name = 'add_student'),
-    path('edit/<int:pk>/', views.StudentUpdate.as_view(), name = 'edit_student'),
+    path('', views.IndexView, name='index'),
+    path('students/', views.StudentList.as_view(), name='student_list'),
+    path('add/', views.StudentCreate.as_view(), name='add_student'),
+    path('edit/<int:pk>/', views.StudentUpdate.as_view(), name='edit_student'),
 ]
