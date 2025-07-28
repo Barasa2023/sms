0d919161030c5235c4a9796173167f8c40803877
Barasa2023 <officialbarasa@gmail.com>
2025-07-28 11:08:44 +0300

    Student Edit template

--- students/urls.py
+++ students/urls.py
@@ -6,4 +6,5 @@ app_name = 'students'
 urlpatterns = [
     path('', views.StudentList.as_view(), name = 'student_list'),
     path('add/', views.StudentCreate.as_view(), name = 'add_student'),
+    path('edit/<int:pk>/', views.StudentUpdate.as_view(), name = 'edit_student'),
 ]
