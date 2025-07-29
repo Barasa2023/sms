cc8afe658b5e05c7b5a8e93d679ff6a55dacad27
Barasa2023 <officialbarasa@gmail.com>
2025-07-29 12:37:39 +0300

    Feat: Update urls

--- students/urls.py
+++ students/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('students/', views.StudentList.as_view(), name='student_list'),
     path('add/', views.StudentCreate.as_view(), name='add_student'),
     path('edit/<int:pk>/', views.StudentUpdate.as_view(), name='edit_student'),
+    path('students/<int:pk>/', views.StudentDetail.as_view(), name='student_detail')
 ]
